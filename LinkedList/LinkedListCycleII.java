f4086928187e4963ef35b91d39193537e584aef5
Aria Li <aria.li@Arias-MacBook-Air.local>
2021-09-14 18:27:14 -0700

    solve Linked List Cycle II problem

---
+++ LinkedList/LinkedListCycleII.java
@@ -0,0 +1,18 @@
+//Link: https://leetcode.com/problems/linked-list-cycle-ii/
+
+// Hash Table solution
+// Time complexity: O(n)
+// Space complexity: O(n)
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        Set<ListNode> nodes = new HashSet<>();
+        while (head.next != null) {
+            if (nodes.contains(head)) {
+                return head;
+            }
+            nodes.add(head);
+            head = head.next;
+        }
+        return null;
+    }
+}
