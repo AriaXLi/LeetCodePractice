09b189c87659f91e24bd0968a0ba7a4dcb6f0a4e
Aria Li <aria.li@Arias-MacBook-Air.local>
2021-09-16 23:49:51 -0700

    solve moving data stream problem

---
+++ StacksAndQueues/MovingAverageDataStream.java
@@ -0,0 +1,22 @@
+//Problem: Moving Average from Data Stream
+// https://leetcode.com/problems/moving-average-from-data-stream/
+
+	class MovingAverageDataStream {
+  int size, head, windowSum, count = 0;
+  int[] queue;
+  public MovingAverage(int size) {
+    this.size = size;
+    queue = new int[size];
+  }
+
+  public double next(int val) {
+    ++count;
+    // calculate the new sum by shifting the window
+    int tail = (head + 1) % size;
+    windowSum = windowSum - queue[tail] + val;
+    // move on to the next head
+    head = (head + 1) % size;
+    queue[head] = val;
+    return windowSum * 1.0 / Math.min(size, count);
+  }
+}
