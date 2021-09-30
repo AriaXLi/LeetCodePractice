ff20ec1eba29374fbdb8887af588581bb84caf59
jubielub <jubielub@gmail.com>
2021-09-29 23:58:44 -0700

    solve LRU cashe problem

---
+++ HashTable/LRUCashe.java
@@ -0,0 +1,23 @@
+class LRUCashe {
+    
+    class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+    }
+    int capacity;
+        
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+    }
+    
+    public int get(int key) {
+           
+    }
+    
+    public void put(int key, int value) {
+           
+    }
+    
+}
