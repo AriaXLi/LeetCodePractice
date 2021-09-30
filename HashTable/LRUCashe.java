810bdb50f3f440feeda6a5bbba8ba3404ddaef9f
jubielub <jubielub@gmail.com>
2021-09-30 00:34:07 -0700

    modify LRU cache problem

--- HashTable/LRUCashe.java
+++ HashTable/LRUCashe.java
@@ -1,4 +1,8 @@
-class LRUCashe {
+// Link: https://leetcode.com/problems/lru-cache/
+// Difficulty: Medium
+// Time complexity: O(1)
+// Space complexity: O(n)
+class LRUCache {
     
     class Node {
         int key;
@@ -6,18 +10,79 @@ class LRUCashe {
         Node prev;
         Node next;
     }
+    
+     private void addNode(Node n) {
+       n.prev = head;
+       n.next = head.next;
+       head.next.prev = n;
+       head.next = n;
+    }
+
+    private void removeNode(Node n) {
+       n.prev.next = n.next;
+       n.next.prev = n.prev;
+    }
+
+    private void moveToHead(Node n) {
+        removeNode(n);
+        addNode(n);
+    }
+
+    private Node popTail() {
+       Node ret = tail.prev;
+       removeNode(ret);
+       return ret;
+    }
+    
     int capacity;
-        
+    int size;
+    Map<Integer, Node> cache = new HashMap<>();
+    private Node head;
+    private Node tail;
+
     public LRUCache(int capacity) {
         this.capacity = capacity;
+        this.size = 0;
+        head = new Node();
+        tail = new Node();
+        head.next = tail;
+        tail.prev = head;
+        
     }
     
     public int get(int key) {
-           
+        Node ret = cache.get(key);
+        if (ret == null) {
+            return -1;
+        }
+        moveToHead(ret);
+        return ret.value;
     }
     
     public void put(int key, int value) {
-           
+        Node n = cache.get(key);
+
+        // node alr exists in cache
+        if (n != null) {
+            n.value = value;
+            moveToHead(n);
+        }
+        
+        // node does not exist in cache, so must be added
+        else {
+            if (size == capacity) {
+                Node removed = popTail();
+                cache.remove(removed.key);
+                size--;
+            }
+            Node added = new Node();
+            added.key = key;
+            added.value = value;
+            addNode(added);
+            cache.put(key, added);
+            size++;
+        }
+
     }
     
 }
