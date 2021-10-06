8100772965f03a1ea0fe2e6a1c18531f93a937b9
jubielub <jubielub@gmail.com>
2021-10-05 23:50:38 -0700

    fixed spacing

--- ArraysAndString/MaxUnitsTruck.java
+++ ArraysAndString/MaxUnitsTruck.java
@@ -7,18 +7,18 @@ import java.util.Arrays;
 
 public class MaxUnitsTruck {
     public int maximumUnits(int[][] boxTypes, int truckSize) {
-        
+
         // sorting by unit size
         Arrays.sort(boxTypes, (a, b) -> Integer.compare(a[1], b[1]));
-        
+
         int boxesLeft = truckSize;
         int totalUnits = 0;
-        
+
         // starting from end of sorted array because they are sorted from least to biggest unit size
         for (int i = boxTypes.length - 1; i >= 0 && boxesLeft > 0; i--) {
             int numBoxes = boxTypes[i][0];
             int numUnits = boxTypes[i][1];
-            
+
             if (numBoxes > boxesLeft) {
                 totalUnits += numUnits * boxesLeft;
                 boxesLeft = 0;
