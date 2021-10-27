fd41d4fd5d00631ae9830a3bc61cecf96040636b
jubielub <jubielub@gmail.com>
2021-10-27 16:37:47 -0700

    solve Find Closest Value in BST problem on AlgoExpert

---
+++ TreesAndGraphs/FindClosestValueinBST.py
@@ -0,0 +1,28 @@
+# Link: https://www.algoexpert.io/questions/Find%20Closest%20Value%20In%20BST
+# Difficulty: Easy
+# Time complexity:
+# Space complexity: 
+def findClosestValueInBst(tree, target):
+    # Write your code here.
+    return findClosestValueInBstHelper(tree, target, tree.value)
+	
+def findClosestValueInBstHelper(tree, target, closest):
+	if tree is None:
+		return closest
+	if abs(target - closest) > abs(target - tree.value):
+		closest = tree.value
+	if target < tree.value:
+		return findClosestValueInBstHelper(tree.left, target, closest)
+	elif target > tree.value:
+		return findClosestValueInBstHelper(tree.right, target, closest)
+	else:
+		return closest
+
+# This is the class of the input tree. Do not edit.
+class BST:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+		
